de7593b8e50a1388da9ef979650fc69869d68beb
asutton1999 <angela.sutton1999@gmail.com>
2022-02-09 22:04:24 -0500

    Added Button. OnClick TextView Updates

--- app/src/main/java/com/example/myyearbook/MainActivity.java
+++ app/src/main/java/com/example/myyearbook/MainActivity.java
@@ -3,12 +3,22 @@ package com.example.myyearbook;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity<i> extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+        public void onClick(View v) {
+            TextView mTextView = (TextView)findViewById(R.id.mytextView);
+            mTextView.setText("Freida is my dog's name");
+
+    };
+
+
+
 }
