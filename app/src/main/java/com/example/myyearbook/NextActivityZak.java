87fa7fd73daf01b6bb28eeb8a608d17891ed30eb
zriyaz <zakriariyaz@gmail.com>
2022-02-14 22:24:37 -0500

    Added my second page and all buttons pointing to next person are commented out so they can be replaced with the names of the activities they pertain to.

--- app/src/main/java/com/example/myyearbook/NextActivityZak.java
+++ app/src/main/java/com/example/myyearbook/NextActivityZak.java
@@ -1,31 +1,26 @@
 package com.example.myyearbook;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-
-public class ZakActivity extends AppCompatActivity {
+public class NextActivityZak extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_zak);
+        setContentView(R.layout.activity_next_zak);
     }
-
-    public void onClick(View v) {
-        Intent i = new Intent(getApplicationContext(), NextActivityZak.class);
-        startActivity(i);
-    }
-
-    /*
+/*
     public void buttonClick(View v) {
         Intent i = new Intent(getApplicationContext(), MainActivity.class);
         startActivity(i);
-    }
 
-    Link your main actvity here OM, just replace MainActvity.class with the name of your actvity
-*/
+        Om, this is where you need to connect your main/first page
+
+    }
+    */
 
 }
