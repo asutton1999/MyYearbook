bb63a103242e75db53d667bb7856f791eb068bcc
zriyaz <zakriariyaz@gmail.com>
2022-02-22 12:56:14 -0500

    Added webview on my next_acitvity

--- app/src/main/java/com/example/myyearbook/MainActivity.java
+++ app/src/main/java/com/example/myyearbook/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.webkit.WebView;
 import android.widget.TextView;
 
 public class MainActivity<i> extends AppCompatActivity {
@@ -13,6 +14,7 @@ public class MainActivity<i> extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 
 
